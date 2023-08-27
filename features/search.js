175d852db6dd02273188f445cf2ccd39fc85f6bc
sauravadiyodi <saurav.adiyodi@gmail.com>
2023-08-27 11:00:09 +0530

    Search API ready

--- features/search.js
+++ features/search.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+let apiKey = process.env.TMDB_API;
 
 // router middleware
 // router.use((req, res, next) => {
@@ -8,8 +10,22 @@ const router = express.Router()
 // })
 
 // query route
-router.get('/:query', (req, res) => {
-    res.send(`Testing query ${req.params.query}`)
+router.get('/:query', async (req, res) => {
+    let query = req.params.query;
+    let searchData = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`);
+    let searchObj = searchData.data.results;
+    let totalRes = searchData.data.total_results;
+    if (totalRes > 5) {
+        searchObj = searchObj.slice(0, 5);
+    }
+    let resultsArr = [];
+    searchObj.forEach(item => {
+        resultsArr.push({
+            title: item.original_title,
+            id: item.id
+        });
+    });
+    res.send(resultsArr);
 })
 
 module.exports = router
